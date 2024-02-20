3ea20e7a08b02a15d2a59cf3c11d49af03acce0f
Adam Raźniewski <adam@razniewski.eu>
2024-02-20 14:13:56 +0100

    bullshit highlight

--- InBrowserTools/src/pages/IndexPage.vue
+++ InBrowserTools/src/pages/IndexPage.vue
@@ -9,7 +9,7 @@
         There is no analytics involved on this application. Feel free and safe to use it. No external server is involved.
       </div>
       <div style="font-size:24px">
-        No shitty ads, no privacy concerns, no tracking, no cookies, no bullshit.
+        No shitty ads, no privacy concerns, no tracking, no cookies, no external server, <span  style="font-size: 26px; color: rgb(21, 255, 44);">no bullshit</span>.
       </div>
     </div>
   </q-page>
