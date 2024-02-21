60e0337183b217c73597fe949bb698a0e49297df
Adam Raźniewski <adam@razniewski.eu>
2024-02-21 16:39:53 +0100

    update

--- InBrowserTools/src/pages/IndexPage.vue
+++ InBrowserTools/src/pages/IndexPage.vue
@@ -8,9 +8,6 @@
       <div class="col-12 row justify-center" style="margin-top: 10px;">
         There is no analytics involved on this application. Feel free and safe to use it.
       </div>
-      <div>
-        Page is hosted on Github Pages
-      </div>
       <div style="font-size:24px">
         No shitty ads, no privacy concerns, no tracking, no cookies, no external server, <span  style="font-weight: 900; font-size: 26px; color: rgb(24, 139, 36);">no bullshit</span>.
       </div>
